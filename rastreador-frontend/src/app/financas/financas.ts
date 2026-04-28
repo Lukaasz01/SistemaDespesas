96816e806ce5181cb0cbb1a8e4de2bff4df8235c
mlucasCFM <mlucas@portalmedico.org.br>
2026-04-28 13:36:22 -0300

    Lógica de componentes montada e lógica do back end recebendo categorias e usuarios

---
+++ rastreador-frontend/src/app/financas/financas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financas',
+  imports: [],
+  templateUrl: './financas.html',
+  styleUrl: './financas.css',
+})
+export class Financas {}
